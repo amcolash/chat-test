37676f3bf3bebff4511ba38f57e82af4678bc069
Andrew McOlash <amcolash@gmail.com>
2015-11-27 23:51:11 -0600

    Add in user id to chat rooms

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var userid = 0;
+
 server.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/bower_components'));
@@ -10,7 +12,9 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+  io.emit('welcome', userid);
+  userid++;
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
