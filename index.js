082a05317621195c5b4c4f2c3e759ff50d3a3525
Andrew McOlash <amcolash@gmail.com>
2015-11-27 23:16:21 -0600

    Simple chat application using socket.io

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/bower_components'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000);
