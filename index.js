bd44dc8f1769d8f77da20b57b0392da6a5511a06
Andrew McOlash <amcolash@gmail.com>
2015-11-28 17:05:48 -0600

    Add online user list functionality

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var HashMap = require('hashmap');
 
-var userid = 0;
+var nextuserid = 0;
+var allClients = new HashMap();
 
 server.listen(process.env.PORT || 5000);
 
@@ -13,10 +15,25 @@ app.get('/', function(req, res,next) {
 });
 
 io.on('connection', function(socket) {
-  io.emit('welcome', userid);
-  userid++;
+  io.emit('welcome', 'user' + nextuserid);
+  allClients.set(socket, 'user' + nextuserid);
+  io.emit('users', allClients.values());
 
-  socket.on('chat message', function(msg){
+  nextuserid++;
+
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
+  });
+
+  socket.on('rename', function(msg) {
+    io.emit('chat message', allClients.get(socket) + ' has changed their name to ' + msg);
+    allClients.set(socket, msg);
+    io.emit('users', allClients.values());
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('chat message', allClients.get(socket) + ' left chat room');
+    allClients.remove(socket);
+    io.emit('users', allClients.values());
   });
 });
--- public/app.js
+++ public/app.js
@@ -7,24 +7,40 @@ $('form').submit(function(){
   $('#m').val('');
   return false;
 });
+
 socket.on('welcome', function(msg){
   if (!connected) {
-    userid = 'user' + msg;
+    userid = msg;
     $('#user').text(userid + ' (You)');
     connected = true;
     $('#messages ul').append($('<li>').text('Welcome to the chatroom ' + userid));
   } else {
-    $('#messages ul').append($('<li>').text('user' + msg + ' has entered the chat room'));
+    $('#messages ul').append($('<li>').text(msg + ' has entered the chat room'));
   }
 });
+
 socket.on('chat message', function(msg){
   $('#messages ul').append($('<li>').text(msg));
+});
+
+socket.on('users', function(msg) {
+  console.log(msg);
+
+  var userIndex = msg.indexOf(userid);
+  if (userIndex > -1) {
+    msg.splice(userIndex, 1);
+  }
+
+  $('#user-list').empty();
+  $.each(msg, function(index, value) {
+    $('#user-list').append($('<li>').text(value));
+  });
 });
 
 $('#rename').click(function() {
   var temp = window.prompt("New User Id?", userid);
   if (temp !== null && temp !== userid) {
-    socket.emit('chat message', userid + ' has changed their name to ' + temp);
+    socket.emit('rename', temp);
     userid = temp;
     $('#user').text(userid + ' (You)');
   }
