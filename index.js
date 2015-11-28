1aff1223abbe3ab40d61452dd562374f49d76a6c
Andrew McOlash <amcolash@gmail.com>
2015-11-27 23:34:15 -0600

    Minor changes to make port configured with ENV vars

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+server.listen(process.env.PORT || 5000);
+
 app.use(express.static(__dirname + '/bower_components'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
@@ -13,5 +15,3 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
-server.listen(3000);
